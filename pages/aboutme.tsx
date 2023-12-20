68ee0c60f2b68708f99550c996a8ab7993adb284
Kornthiwa Wirata <kornthiwa.wir@everapp.io>
2023-12-20 11:00:22 +0700

    no message

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,55 +1,102 @@
-import * as React from 'react';
-import { Box, ThemeProvider, createTheme } from '@mui/system';
+import * as React from "react";
+import styled from "@mui/system/styled";
+import Grid from "@mui/system/Unstable_Grid";
+import Box from "@mui/system/Box";
+import { ImageList, ImageListItem } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    background: {
-      paper: '#fff',
-    },
-    text: {
-      primary: '#173A5E',
-      secondary: '#46505A',
-    },
-    action: {
-      active: '#001E3C',
-    },
-    success: {
-      dark: '#009688',
-    },
-  },
-});
+const Item = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  border: "1px solid",
+  borderColor: theme.palette.mode === "dark" ? "#444d58" : "#ced7e0",
+  padding: theme.spacing(1),
+  borderRadius: "4px",
+  textAlign: "center",
+  width: "400px",
+}));
 
-export default function Example() {
+// Main component
+export default function RowAndColumnSpacing() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          boxShadow: 1,
-          borderRadius: 2,
-          p: 2,
-          minWidth: 300,
-        }}
-      >
-        <Box sx={{ color: 'text.secondary' }}>Sessions</Box>
-        <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
-          98.3 K
-        </Box>
-        <Box
-          sx={{
-            color: 'success.dark',
-            display: 'inline',
-            fontWeight: 'bold',
-            mx: 0.5,
-            fontSize: 14,
-          }}
-        >
-          +18.77%
-        </Box>
-        <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 14 }}>
-          vs. last week
-        </Box>
-      </Box>
-    </ThemeProvider>
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid container rowSpacing={1} spacing={1}>
+        <Grid xs={12} sx={{ justifyContent: "center", alignItems: "center" }}>
+          <Box
+            sx={{
+              width: "auto",
+              height: "auto",
+              justifyContent: "center",
+              display: "flex",
+            }}
+          >
+            <Box sx={{ border: "1px solid", p: 0.5, borderRadius: "4px" }}>
+              <ImageList sx={{ width: 300, height: 300 }} cols={2}>
+                {itemData.map((item) => (
+                  <ImageListItem key={item.img}>
+                    <img
+                      srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                      alt={item.title}
+                      loading="lazy"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid xs={6} sx={{ justifyContent: "right", display: "grid" }}>
+          <Item>ชื่อ:</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item sx={{ textAlign: "left" }}>Kornthiwa Wirata</Item>
+        </Grid>
+        <Grid xs={6} sx={{ justifyContent: "right", display: "grid" }}>
+          <Item>ที่อยู่:</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item sx={{ textAlign: "left" }}>PEN UDONTHANI</Item>
+        </Grid>
+        <Grid xs={6} sx={{ justifyContent: "right", display: "grid" }}>
+          <Item>เรียนที่:</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item sx={{ textAlign: "left" }}>UDON THANI RAJABHAT UNIVERSITY</Item>
+        </Grid>
+        <Grid xs={6} sx={{ justifyContent: "right", display: "grid" }}>
+          <Item>เบอร์โทร:</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item sx={{ textAlign: "left" }}>0958705098</Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+];
